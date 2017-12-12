7d19d3f19460dde759dfdb436eddee08509705ce
manos <manos-mark@hotmail.com>
2017-12-12 16:24:44 +0200

    Finished

--- js/script.js
+++ js/script.js
@@ -17,82 +17,106 @@ $(document).ready(function() {
 	//add action listener to my-fixed-button
 	$('#my-fixed-button a').on('click', openNewModal);
 	$('#createButton').on('click', createNewModal);	
+
 });
 
 
 
 //collect data from Json and display cards
-function loadData(){
+async function loadData(){
 	// Retrieve the template data from the HTML (jQuery is used here).
 	template = $('#entry-template').html();
 	// Compile the template loadData into a function
 	templateScript = Handlebars.compile(template);
 
-	//find all cards from Cards List
-	Trello.get("/lists/" + '5a22e582182a8984a90ebcdb' + "/cards", function (resp) {
-		var id = 0;
-		resp.forEach(function(d){
+	var cards = await findCards();
 
-			//find all attachments from each card (it works only if we have 1 attachment)
-			var imageUrl;
-			Trello.get('cards/' +d.id+ '/attachments', function(attachments){
+	var id = 0;
+	for( let card of cards ){
+		//find all attachments from each card (it works only if we have 1 attachment)
+		await Trello.get('cards/' +card.id+ '/attachments', function(attachments){
+			
+			if( attachments[0] != null ){
 				// Use the data that we need with handlebars
-				var context = { "id": id, "title": d.name, "body": d.desc, "image": attachments[0].url };
-				var html = templateScript(context);
-				// Insert the HTML code into the page
-				$("#entry-template").append(html);
-				//append the new card
-				$("#main").append(html);
-				//if image does not exist then use an error image
-				$("img").on('error', function() { 
-					$(this).attr("src", 'http://indembassy.com.vn/images/error.png');
-				});
-			})
-			  
+				var context = { "id": id, "title": card.name, "body": card.desc, "image": attachments[0].url };
+			}else{
+				var context = { "id": id, "title": card.name, "body": card.desc, "image": 'http://indembassy.com.vn/images/error.png' };
+				Trello.post( "/cards/" + cards[id].id + "/attachments/", { url:'http://indembassy.com.vn/images/error.png' });	
+			}	
+
+			var html = templateScript(context);
+			// Insert the HTML code into the page
+			$("#entry-template").append(html);
+			//append the new card
+			$("#main").append(html);
 			id++;
-		})
 
-      	lastId = countId = id; //count is equal with the last id given
-		//set the quotes count 
-		$('#count').text(countId);
+		});
 
-		loadPagination(countId);//needs to be done after the card elements are ready
+		//if image does not exist then use an error image
+		$("img").on('error', function() { 
+			$(this).attr("src", 'http://indembassy.com.vn/images/error.png');
+		});
 
-    }, function (error) { console.log("error:", error) });
+		countId = lastId = id; //count is equal with the last id given
+	}
+	
+	$('.cards').masonry({
+    	itemSelector: '.col',
+	});
+	//set the quotes count 
+	$('#count').text(countId);
+	loadPagination(countId);//needs to be done after the card elements are ready
 }
 
+//find all cards from Cards List
+async function findCards(){
+	var cards = await Trello.get("/lists/" + '5a22e582182a8984a90ebcdb' + "/cards", function (list) {
+		cards = list.slice();
+	});
+	return cards;
+}
 
 //open delete modal
 function openDeleteModal(){
 	//find which element trigered the modal 
 	//$(this) is the button
 	//find the root parent 
-	modalTriger = $(this).parents('.card').parent();
+	modalTriger = $(this).parents('.cell');
 	// //open modal
 	$('#modalDelete').modal('open');
 }
 
 //remove card 
-function deleteCard(){
+async function deleteCard(){
 	$(modalTriger).remove();
+
 	Materialize.toast('Card ' + modalTriger.attr('id') + ' Deleted!', 4000);
 	$('#count').text(--countId);//decrease count and display
+
+	//archive the card on Trello
+	var cards = await findCards();
+	Trello.put("/cards/" + cards[modalTriger.attr('id')].id + '/closed?value=true');
+	
 }
 
 
 
 //open format modal
-function openFormatModal(){
+async function openFormatModal(){
 	//find which element trigered the modal 
 	//find the root parent 
-	modalTriger = $(this).parents('.card').parent();
+	modalTriger = $(this).parents('.cell');
 
 	//open modal
 	$('#modalFormat').modal('open');
 
-	//find the children with class resizeimg and get the src attribute
-	trigerUrl = $(modalTriger).find('.resizeimg').attr('src');
-	$('#image_url').val(trigerUrl);
+	var cards = await findCards();
+	Trello.get("/cards/"+cards[modalTriger.attr('id')].id+"/attachments/",
+		function (r) {
+			trigerUrl=r[0].url;
+			$('#image_url').val(trigerUrl);
+	});
 
 	// find the children with class card-title and get the value
 	trigerTitle = $(modalTriger).find('.card-title').text();
@@ -105,7 +129,7 @@ function openFormatModal(){
 }
 
 //format modal
-function formatCard(){
+async function formatCard(){
 	//find the children with class resizeimg and set the src attribute
 	$(modalTriger).find('.resizeimg')
 								.attr( 'src',$('#image_url').val() );
@@ -115,10 +139,21 @@ function formatCard(){
 	// //find the children with class card-content and set the text	
 	$(modalTriger).find('.card-content').children()
 								.text( $('#image_description').val() );
-
 	Materialize.toast('Card ' + modalTriger.attr('id') + ' Formated!', 4000);	
 
-}
+	//put description and title
+	var cards = await findCards();
+	Trello.put("/cards/" + cards[modalTriger.attr('id')].id,
+		{ desc:$('#image_description').val(), name:$('#image_title').val()});
+
+	//--delete attachment--//
+	await Trello.get("/cards/" + cards[modalTriger.attr('id')].id + "/attachments/",function (a) {
+		Trello.delete("/cards/" + cards[modalTriger.attr('id')].id + "/attachments/" + a[0].id);
+	});
+
+	//--create new attachment--//
+	Trello.post("/cards/" + cards[modalTriger.attr('id')].id + "/attachments/",{url:$('#image_url').val()});		
+}	
 
 
 
@@ -136,7 +171,11 @@ function createNewModal(){
 	var getAuthor = $('#add_image_title').val();
 	var getQuote = $('#add_image_description').val();
 
-	var context = { "id": lastId++ ,"title":getAuthor, "body":getQuote, "image":getImage };
+	Trello.post("/cards/", { name: getAuthor, desc: getQuote,idList: "5a22e582182a8984a90ebcdb" }, function (resp) {
+		Trello.post("/cards/"+resp.id+"/attachments/",{url:getImage});
+    });
+
+	var context = { "id": lastId ,"title":getAuthor, "body":getQuote, "image":getImage };
 	var html = templateScript(context);
 
 	//append the new card
@@ -146,9 +185,12 @@ function createNewModal(){
 		$(this).attr("src", 'http://indembassy.com.vn/images/error.png');
 	});
 
+	$('#'+lastId).fadeIn('slow');
+
 	//set the quotes count 
 	$('#count').text(++countId);
 	Materialize.toast('New Card ' + lastId + ' Created!', 4000);
+	lastId++;
 	//----Clear data-----//
 	clearData();
 }
@@ -173,6 +215,13 @@ function loadPagination(cnt){
 			setCardsInvisible();
 			setCardsVisible(0, cardsPerPage-1);
 			$('#leftArrow').addClass('disabled');
+
+			//if there is only one page disable the arrow 
+			if( $('.pagination li').length == 3){// 3 li (two arrows and one page)
+				$('#rightArrow').addClass('disabled');
+			}else{
+				$('#rightArrow').removeClass('disabled');
+			}
 	});
 
 	//add rest list elements and listeners
@@ -186,6 +235,7 @@ function loadPagination(cnt){
 			setCardsVisible(i*cardsPerPage-cardsPerPage, i*cardsPerPage-1);
 			$('#leftArrow').removeClass('disabled');
 			
+			//if is the last page disable the arrow
 			if( $(this).attr('id') == 'p'+howManyPages ){//if this is the last page
 				$('#rightArrow').addClass('disabled');
 			}else{
@@ -193,17 +243,12 @@ function loadPagination(cnt){
 			}
 
 		});
-		// console.log('range: '+(i*cardsPerPage-cardsPerPage)+' '+(i*cardsPerPage-1));	
 	}
 
-	//if there is only one page set right arrow disable
-	if( $('.pagination li').length == 2){// 3 li (two arrows and one page)
-		$('rightArrow').addClass('disabled');
-	}else{
-		$('rightArrow').removeClass('disabled');
-	}
+	//add the right arrow
+	$('.pagination').append('<li id="rightArrow" class="waves-effect"><a href="#"><i class="material-icons">chevron_right</i></a></li>');
 
-	//triger to reveal the first 20 elements
+	//triger to reveal the first elements
 	$('#p1').click();
 	
 	//left arrow listener
@@ -225,9 +270,6 @@ function loadPagination(cnt){
 			$(currentActive).next().children().click();
 		}
 	});
-
-	//add the right arrow
-	$('.pagination').append('<li id="rightArrow" class="waves-effect"><a href="#"><i class="material-icons">chevron_right</i></a></li>');
 	
 
 	function setActive(p){
@@ -245,7 +287,7 @@ function loadPagination(cnt){
 
 	function setCardsInvisible(){
 		$('.cell').each(function(i,obj){
-			$(obj).fadeOut('slow');
+			$(obj).css('display','none');
 		});
 	}
 
